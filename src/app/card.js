812bdddf26a8a2b3b775ba15526ff4a8adf4a2dc
EffexDev <effexdev@gmail.com>
2025-03-21 23:00:00 +0800

    init repo

---
+++ src/app/card.js
@@ -0,0 +1,7 @@
+export default function Card() {
+    return (
+        <div className="flex flex-col border border-gray-700 shadow-black flex-1">
+            <h1>test</h1>
+        </div>
+    )
+}
---
+++ src/app/layout.js
@@ -0,0 +1,29 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Effex | Dashboard",
+  description: "A function browser dashboard.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
