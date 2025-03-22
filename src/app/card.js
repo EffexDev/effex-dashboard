a3aa65184ce7fcb8fb62b65d6801ec21771006ef
EffexDev <effexdev@gmail.com>
2025-03-22 14:03:45 +0800

    Card and button done

--- src/app/card.js
+++ src/app/card.js
@@ -2,8 +2,9 @@ import AnimatedButton from "./AnimatedButton";
 
 export default function Card({title, content, image, altText, buttonText, href}) {
     return (
-        <div className="relative flex flex-col justify-evenly p-5 text-xl border border-gray-700 shadow-black h-50 flex-1 w-full rounded-2xl appearance-none bg-transparent text-left   hover:bg-gray-100">
-            <img src={image} alt={altText} className="max-w-10" />
+        <div className="relative flex flex-col h-50 flex-1 justify-evenly p-5 text-xl bg-white/30 rounded-2xl shadow-2xl backdrop-blur-sm border border-black/50
+">
+            <img src={image} alt={altText} className="max-w-10 absolute top-4 right-4 rounded-lg" />
             <h1>{title}</h1>
             <p>{content}</p>
             <AnimatedButton href={href}>{buttonText}</AnimatedButton>
--- src/app/page.js
+++ src/app/page.js
@@ -1,24 +1,25 @@
 import Card from "./card";
-import DeepSeek from "./DeepSeek";
 import Greeting from "./Greeting";
 
 export default function Home() {
   return (
-    <div className="relative flex flex-col h-screen w-full items-center justify-center overflow-hidden">
-      <div>
+    <div className="relative flex flex-col w-full items-center sm:h-screen justify-center py-5">
+      <div className="w-[90%] sm:w-[80%]">
         <Greeting />
-        <div className="relative z-10 flex flex-col items-center justify-center">
-          <div className="w-[80vw] flex flex-row gap-4">
+        <div className="relative grid grid-cols-1 sm:grid-cols-3 gap-4 items-center justify-center">
+            <Card href="https://jordancartledge.com.au "title="Portfolio" content="My web dev portfolio" image="/effexForge.jpg" buttonText="See my work" altText="Jordan Cartledge's Dev profile"/>
+
+            <Card href="https://effexforge.com" title="Effex Forge" image="/effexForge.jpg" content="Building strong online foundations" buttonText="Make your mark" altText="Effex Forge logo"/>
+
+            <Card href="https://scriptjar.jordancartledge.com.au" title="script.jar" content="A template management platform" buttonText="Open the jar" altText="🫙"/>
+
+            <Card href="https://github.com/EffexDev" title="Github" content="My code repo" image="/github.png" buttonText="Merge together" altText="Github logo"/>
+
             <Card title="Youtube" content="Video watching platform"/>
-            <Card title="Youtube" content="Video watching platform"/>
-            <Card href="https://youtube.com" title="Youtube" content="Video watching platform" image="/youtube.png" buttonText="Watch videos" altText="Youtube Logo"/>
-          </div>
-          <div className="w-[80vw] flex flex-row gap-4 mt-4">
-             <Card title="Youtube" content="Video watching platform"/>
-            <Card title="Youtube" content="Video watching platform"/>
+
             <Card title="Youtube" content="Video watching platform" image="/youtube.png" buttonText="Watch videos" altText="Youtube Logo"/>
-          </div>
-        </div>
+
+            </div>
       </div>
     </div>
   );
